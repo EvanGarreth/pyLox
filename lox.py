492e985c0e8a26aa4ea78fa7dce0f811e3827c3b
Evan Campbell <evangarreth@gmail.com>
2020-11-23 12:13:24 -0600

    Add base interpreter framework

---
+++ lox.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+
+def run_file(filename):
+    filename = Path(filename)
+    with open(filename, 'r') as file:
+        data = file.read()
+    run(data)
+
+def run_prompt():
+    while True:
+        data = input("> ")
+        if data == "":
+            break
+        run(data)
+
+def run(data):
+    tokens = data.split()
+    for token in tokens:
+        print(token)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print(f"Usage: {sys.argv[0]} [script]")
+    elif len(sys.argv) == 2:
+        run_file(sys.argv[1])
+    else:
+        run_prompt()
+
+    
